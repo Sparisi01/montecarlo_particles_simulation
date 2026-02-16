16529562b963c889c6275bc07106450ebe68b632
Sparisi01 <sparisi2002@gmail.com>
2026-02-16 10:42:57 +0100

    readme modified

--- README.md
+++
@@ -1,3 +0,0 @@
-# montecarlo_particles_simulation
-
-BUILD: gcc ./file.c -o ./build/file -lm -Wall -std=gnu17 -O3
--- complexity_study.c
+++ complexity_study.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 #include <string.h>
 
 #include "src/constants.c"
